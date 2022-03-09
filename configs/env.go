a9b532741f3cb8d94db665208d52005d43fd4734
olususi oluyemi <yemiwebby@gmail.com>
2022-03-09 21:35:16 +0100

    Removed env check

--- configs/env.go
+++ configs/env.go
@@ -1,20 +1,17 @@
 package configs
 
 import (
-	"log"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func EnvMongoURI() string {
-    if os.Getenv("APP_ENV") != "production" {
-        err := godotenv.Load()
-        if err != nil {
-            log.Fatal("Error loading .env file within the application")
-        }
+    // if os.Getenv("APP_ENV") != "production" {
+    //     err := godotenv.Load()
+    //     if err != nil {
+    //         log.Fatal("Error loading .env file within the application")
+    //     }
       
-    }
+    // }
 
     return os.Getenv("MONGOURI")
 }
