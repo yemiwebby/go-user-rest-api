7e8394d4bc227dcbf0eb3463a4352539f6e0b043
olususi oluyemi <yemiwebby@gmail.com>
2022-03-09 19:55:55 +0100

    ssf

--- configs/env.go
+++ configs/env.go
@@ -8,10 +8,13 @@ import (
 )
 
 func EnvMongoURI() string {
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading .env file within the application")
+    if os.Getenv("APP_ENV") != "production" {
+        err := godotenv.Load()
+        if err != nil {
+            log.Fatal("Error loading .env file within the application")
+        }
+      
     }
-  
+
     return os.Getenv("MONGOURI")
 }
